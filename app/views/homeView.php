2d724c2ce61e6f90d179d065a0cab0b6c5d5cdfa
OlmeLau <olmelau@gmail.com>
2026-03-10 13:57:25 +0100

    mostrar home

---
+++ app/views/homeView.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SERCAMPO DATABASE</title>
+</head>
+<body>
+    
+    <h1>Bienvenido a la base de datos de sercampo</h1>
+    <p>Esto es homeView.php</p>
+
+    <form action="index.php">
+        <input type="hidden" name="controller" value="login">
+        <input type="hidden" name="action" value="procesarLogin">
+        <label for="usuario">Usuario: </label>
+        <input type="text" name="usuario" id="usuario">
+        <label for="password">Contraseña: </label>
+        <input type="password" name="password" id="password">
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
