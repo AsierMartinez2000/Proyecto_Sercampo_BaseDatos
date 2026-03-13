822555e56fb5ae1e924a00f71ea5fd5e64d36049
OlmeLau <olmelau@gmail.com>
2026-03-13 09:48:56 +0100

    hecho un buscador autocompletado

--- app/views/dashBoardView.php
+++ app/views/dashBoardView.php
@@ -11,12 +11,15 @@
     <h3>Bienvenido <?php echo $_SESSION['nombre']; ?> </h3>
 
     <!-- AQUI SE MOSTRARA LA INFORMACION NECESARIA PARA HACER LAS CONSULTAS -->
-
+    <form action="index.php" method="post">
+        <input type="hidden" name="controller" value="productos_test">
+        <input type="hidden" name="action" value="mostrarBuscador">
+        <button type="submit">Buscar producto</button>
+    </form>
     <form action="index.php" method="post">
         <input type="hidden" name="controller" value="login">
         <input type="hidden" name="action" value="logout">
         <button type="submit">Cerrar sesión</button>
     </form>
-
 </body>
 </html>
