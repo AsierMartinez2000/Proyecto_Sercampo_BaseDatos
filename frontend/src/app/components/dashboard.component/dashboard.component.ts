bc84b67ed7ae77a60cd0359794e04a7ce01bed92
AsierMartinez2000 <asiermm2000@gmail.com>
2026-03-24 12:42:36 +0100

    Eliminado error de la consola, carga todos de primeras

--- frontend/src/app/components/dashboard.component/dashboard.component.ts
+++ frontend/src/app/components/dashboard.component/dashboard.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, AfterViewInit, ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit, AfterViewInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ClientesService } from '../../services/clientes.service';
 import { FormsModule } from '@angular/forms';
-import { isEmpty } from 'rxjs';
+
 
 
 @Component({
@@ -14,39 +14,33 @@ import { isEmpty } from 'rxjs';
 })
 export class DashboardComponent implements OnInit{  //implements OnInit, AfterViewInit{
 
-    //El atributo de nuestro componente es un array.
+  //El atributo de nuestro componente es un array.
   //Queremos guardar aquí todos los clientes cuando nos lleguen del backend.
-
-  clientes: any[] = [];
+  
   cliente = {nombre: ""};
+  clientes: any[] = [];
 
-  timeoutId: any;
-  isActive: boolean = true;
-
- 
   //Para usar los metodos de los "componentes" importados que vienen por defecto en Angular
   //Tendremos que inicializar el atributo que se refiere a ellos.
   constructor(
     private router: Router,
     private clienteService: ClientesService,
-    private cd: ChangeDetectorRef
   ){}
 
-  // // Esto carga al final de cargar el componente
+  
+  // Esto carga al final de cargar el componente
+  ngOnInit() {
 
- ngOnInit() {
-
-    this.cargarClientesPorNombre();
-    // Llamar aquí los métodos que vengan del servicios
-    // this.recuperarClientes(); 
-    // this.clientes = this.clienteService.obtenerClientes();
-    
-    // console.log(this.clientes);
-    // console.log(this.datos);
-    this.ejecutarRecursivamente();
+    this.cargarClientes()
 
   }
 
+  //Este metodo carga todos los clientes, va en OnInit, porque la primera vez que entra carga todos.
+  cargarClientes(){
+    this.clientes = this.clienteService.obtenerClientes();
+  }
+
+  //Metodo que se llama al escribir en el INPUT. Busca en base al nombre con LIKE.
   cargarClientesPorNombre(){
     this.clienteService.obtenerClienteNombre(this.cliente).subscribe(
       (resultado:any) =>{
@@ -55,21 +49,6 @@ export class DashboardComponent implements OnInit{  //implements OnInit, AfterVi
       
   });
   }
-
-  async ejecutarRecursivamente(iteracion: number = 1) {
-    if (!this.isActive ) return;
-    
-    if (iteracion > 2) {
-
-        return;
-    }
-    
-    await this.cargarClientesPorNombre();
-    
-    this.timeoutId = setTimeout(() => {
-        this.ejecutarRecursivamente(iteracion + 1);
-    }, 1000);
-}
 
   // Esto se carga después de que la vista (HTML) del componente y sus hijos ya están renderizados en el DOM
   // ngAfterViewInit(){
