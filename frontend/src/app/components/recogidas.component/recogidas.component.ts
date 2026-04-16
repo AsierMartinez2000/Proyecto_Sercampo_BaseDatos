9a88f36597cfb5115012e5baba2e2a4cd8a69971
AsierMartinez2000 <asiermm2000@gmail.com>
2026-04-16 11:06:09 +0200

    Buscador funcional

--- frontend/src/app/components/recogidas.component/recogidas.component.ts
+++ frontend/src/app/components/recogidas.component/recogidas.component.ts
@@ -66,14 +66,31 @@ export class RecogidasComponent {
   ) {}
 
   buscarHoreca(){
+    if((this.horeca_buscado.nombre.length) >= 3){
     this.recogidaService.traerHorecasPorNombre(this.horeca_buscado).subscribe((resultado: any) => {
       this.horecas = resultado; //Esto es el array
       this.cdr.detectChanges();
       console.log(resultado);
     });
+    }
+    else{
+      this.horecas = [];
+    }
   }
 
-  seleccionarHoreca(horeca: any) {
+  seleccionarHoreca(horeca: any) { //Este Horeca es el seleccionado dentro del array Horecas, de la lista.
+
+    this.recogida_nueva.nombre = horeca.nombre;
+    this.recogida_nueva.cif = horeca.cif;
+    this.recogida_nueva.telefono = horeca.telefono;
+    this.recogida_nueva.direccion = horeca.direccion;
+    this.recogida_nueva.municipio = horeca.municipio;
+    this.recogida_nueva.cod_postal = horeca.cod_postal;
+    this.recogida_nueva.provincia = horeca.provincia;
+    this.recogida_nueva.pais = horeca.pais;
+
+    this.horeca_buscado.nombre = "";
+    this.buscarHoreca();
 
   }
 
@@ -96,6 +113,42 @@ export class RecogidasComponent {
     } else {
       console.log('no hay nada que recoger');
     }
+  }
+
+  limpiarFormulario(){
+    
+    //datos del contenedor
+    this.recogida_nueva.id_contenedor = "",
+    this.recogida_nueva.fecha = "",
+    this.recogida_nueva.id_ruta = "",
+    this.recogida_nueva.litros_recogidos = "",
+    this.recogida_nueva.visitado = true,
+    this.recogida_nueva.recogida = true,
+    this.recogida_nueva.bidones_recogidos = "",
+    this.recogida_nueva.bidones_entregados = "",
+    this.recogida_nueva.notas = "",
+    //datos del producto
+    this.recogida_nueva.desengrasante = "",
+    this.recogida_nueva.fregasuelo = "",
+    this.recogida_nueva.lavavajilla = "",
+    this.recogida_nueva.jabon_manos = "",
+    this.recogida_nueva.higienizante = "",
+    this.recogida_nueva.wc_banos = "",
+    this.recogida_nueva.limpia_cristales = "",
+    this.recogida_nueva.lejia = "",
+    this.recogida_nueva.bayeta = "",
+    this.recogida_nueva.filtros = "",
+    this.recogida_nueva.dinero = "",
+    //datos del cliente
+    this.recogida_nueva.nombre = "",
+    this.recogida_nueva.cif = "",
+    this.recogida_nueva.telefono = "",
+    this.recogida_nueva.direccion = "",
+    this.recogida_nueva.municipio = "",
+    this.recogida_nueva.cod_postal = "",
+    this.recogida_nueva.provincia = "",
+    this.recogida_nueva.pais = ""
+
   }
 
   //este método es para que el boton de "HORECA" "RESPOL", "CONTENEDOR" se quede marcado y mostrar el formulario correspondiente
