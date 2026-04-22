3c5624ea5d48c606b11dd44ea351057fd1cf49c3
OlmeLau <olmelau@gmail.com>
2026-04-22 11:55:27 +0200

    formulario de rutas añade vehiculos

--- backend/app/models/rutasModel.php
+++ backend/app/models/rutasModel.php
@@ -36,7 +36,7 @@ class rutasModel{
     }
 
     public function getConductores($datoConductor){
-        $sql = "SELECT id_conductor, nombre
+        $sql = "SELECT id_conductor, nombre, email
                 FROM conductores
                 WHERE nombre LIKE :datoConductor";
 
