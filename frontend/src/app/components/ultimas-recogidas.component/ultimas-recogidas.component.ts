ebb6c44df83e0f7b3d89c7ed28b7a50e713a4ec1
OlmeLau <olmelau@gmail.com>
2026-04-27 11:58:14 +0200

    ideas para las ultimas recogidas

--- frontend/src/app/components/ultimas-recogidas.component/ultimas-recogidas.component.ts
+++ frontend/src/app/components/ultimas-recogidas.component/ultimas-recogidas.component.ts
@@ -1,9 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Pipe } from '@angular/core';
 
 @Component({
   selector: 'app-ultimas-recogidas.component',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './ultimas-recogidas.component.html',
   styleUrl: './ultimas-recogidas.component.css',
 })
-export class UltimasRecogidasComponent {}
+export class UltimasRecogidasComponent {
+  fecha:Date = new Date();
+
+}
