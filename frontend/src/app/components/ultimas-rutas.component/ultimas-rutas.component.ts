835a5735c6f892c2d7f2ceeaef94ed9ad36659e1
AsierMartinez2000 <asiermm2000@gmail.com>
2026-05-06 13:07:50 +0200

    Ya se calculan las estadisticas de ultimas rutas

--- frontend/src/app/components/ultimas-rutas.component/ultimas-rutas.component.ts
+++ frontend/src/app/components/ultimas-rutas.component/ultimas-rutas.component.ts
@@ -2,6 +2,7 @@ import { DatePipe, CommonModule } from '@angular/common';
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RutasService } from '../../services/rutas.service';
+import { RecogidasService } from '../../services/recogidas.service';
 
 @Component({
   selector: 'app-ultimas-rutas.component',
@@ -15,10 +16,11 @@ export class UltimasRutasComponent implements OnInit{
     fecha : new Date()
   };
 
+  array_conductores: any[] = [];
+
   estadisticas = {
     rutas_totales: 0,
     lugares_totales: 0,
-    rutas_Asier: 0,
   };
 
   array_rutas: any[] = [];
@@ -26,42 +28,62 @@ export class UltimasRutasComponent implements OnInit{
   constructor(
     private cdr: ChangeDetectorRef,
     private rutasService: RutasService,
+    private recogidasService: RecogidasService
   ) {
     this.datos.fecha.setMonth(this.datos.fecha.getMonth() - 1);
   }
 
   ngOnInit() {
     this.cargarRutas();
-    this.cdr.detectChanges();
-    this.calcularEstadisticas(); 
+    this.cargarSelects();
+    console.log(this.array_conductores);
     this.cdr.detectChanges();
   }
 
   cargarRutas(){
     this.rutasService.obtenerRutas(this.datos).subscribe((resultado: any) => {
-      console.log(resultado);
       this.array_rutas = resultado;
-      this.cdr.detectChanges();      
+      this.calcularEstadisticas();      
     });
+  }
+
+  cargarSelects(){
+    this.recogidasService.cargarConductores().subscribe((resultado: any) => {
+      this.array_conductores = resultado;
+  })
   }
 
   calcularEstadisticas(){
 
-    this.estadisticas.rutas_Asier = 0;
-
     this.estadisticas.rutas_totales = 0;
+
   
     for(let ruta of this.array_rutas){
 
       this.estadisticas.rutas_totales++;
 
-      if(ruta.nombre_conductor == "Asier"){
-        this.estadisticas.rutas_Asier++;
-      }
+      // if(ruta.nombre_conductor == "Asier"){
+      //   this.estadisticas.rutas_Asier++;
+      // }
 
     }
       this.cdr.detectChanges();
     console.log(this.estadisticas.rutas_totales);
+  }
+
+  calcularPrueba(id_conductor:any): Number{
+
+    let total_conductor = 0;
+
+    for(let ruta of this.array_rutas){
+
+      if(ruta.id_conductor == id_conductor){
+        total_conductor++;
+      }
+
+    }
+
+    return total_conductor;
   }
 
 
