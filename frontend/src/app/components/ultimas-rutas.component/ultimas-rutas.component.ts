07dc8e8508d3920b317320e471d3b1613ee99c04
OlmeLau <olmelau@gmail.com>
2026-05-12 11:49:56 +0200

    cada clase de tipo_legal se muestra de un color diferente en ultimas rutas

--- frontend/src/app/components/ultimas-rutas.component/ultimas-rutas.component.ts
+++ frontend/src/app/components/ultimas-rutas.component/ultimas-rutas.component.ts
@@ -109,5 +109,19 @@ export class UltimasRutasComponent implements OnInit{
     return total_conductor;
   }
 
+  
+  getTipoClass(tipo_legal:string): String{
+     switch (tipo_legal) {
+        case 'Horeca':
+            return 'horeca';
+        case 'EESS Repsol':
+            return 'eess';
+        case 'Contenedor':
+            return 'contenedor';
+        default:
+            return 'tipo-default'; 
+    }
+
+  }
 
 }
