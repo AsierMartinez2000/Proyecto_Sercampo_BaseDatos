e8d0a4f7206a4c715f938fdbcb1da8a2a87b40b5
AsierMartinez2000 <asiermm2000@gmail.com>
2026-05-13 10:33:01 +0200

    Arreglado script provincia no nulo

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -124,7 +124,7 @@ CREATE TABLE zonas (
 CREATE TABLE municipios (
         id_municipio INT PRIMARY KEY AUTO_INCREMENT,
         municipio VARCHAR(50) NOT NULL,
-        provincia VARCHAR(50),
+        provincia VARCHAR(50) NOT NULL,
         pais VARCHAR(50),
         id_zona INT DEFAULT NULL,
         FOREIGN KEY (id_zona) REFERENCES zonas (id_zona) ON DELETE SET NULL
