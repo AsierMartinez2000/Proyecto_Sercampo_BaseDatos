2e4f6dcf8f3b8d0a19b3c17b804bfb28db7a3763
AsierMartinez2000 <asiermm2000@gmail.com>
2026-05-14 17:36:24 +0200

    Añadidos RenderMode En app.routes.server

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -4,5 +4,20 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: 'cliente/:id_cliente',
+    renderMode: RenderMode.Server  
+  },
+
+  {
+    path: 'confirmado/:id_cliente',
+    renderMode: RenderMode.Server  
+  },
+
+  {
+    path: 'modificar/:id_cliente',
+    renderMode: RenderMode.Server  
   }
 ];
